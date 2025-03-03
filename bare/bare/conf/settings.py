0b8296de271d48be04fde91488733298b1a23f94
Robert Hafner <tedivm@users.noreply.github.com>
2025-03-03 11:05:28 +0000

    Updating Templates

--- bare/bare/conf/settings.py
+++ bare/bare/conf/settings.py
@@ -1,3 +1,6 @@
-class Settings:
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
     project_name: str = "bare"
     debug: bool = False
--- library/library/conf/settings.py
+++ library/library/conf/settings.py
@@ -1,3 +1,6 @@
-class Settings:
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
     project_name: str = "library"
     debug: bool = False
